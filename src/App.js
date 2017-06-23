761576f5c55550b55371ba1c8a42ddf897335088
Brandon Cabael <brandoncabael@users.noreply.github.com>
2017-06-22 19:14:12 -0600

    further editing

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ class App extends Component {
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to CMR1!&reg;</h2>
+          <h2>Welcome to CMR1!<sup>&reg;&copy;</sup></h2>
         </div>
         <p className="App-intro">
           Welcome to CMR1&reg; homepage!
